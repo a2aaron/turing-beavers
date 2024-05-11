8125acfbe932b6be069a55a1690ec17f7de9ad9c
Aaron Kofsky <aaronko@umich.edu>
2024-05-11 16:11:25 -0400

    Add comments.

--- src/seed.rs
+++ src/seed.rs
@@ -1,17 +1,33 @@
 use crate::turing::{Action, State, Symbol, Table, Tape, Transition};
 
+/// The number of steps that [BB5_CHAMPION] runs for before halting.
 const TIME_LIMIT: usize = 47_176_870;
+/// The number of unique cells visited by the [BB5_CHAMPION]. Note that this is not how many ones
+/// that the champion writes to the tape, rather it's every cell written to (even cells which are
+/// written to but do not have their value changed). Hence, this will be larger than the number of
+/// ones written)
 const SPACE_LIMIT: usize = 12_289;
+/// The current contender champion for the 5-State 2-Symbol Busy Beaver.
 const BB5_CHAMPION: &str = "1RB1LC_1RC1RB_1RD0LE_1LA1LD_1RZ0LA";
+/// The starting machine to use when doing the phase 1 seed generation. This is the machine with
+/// all empty transitions, except for the "state A + symbol 0" transition, which is
+/// "write 1 + move right + state b".
 const STARTING_MACHINE: &str = "1RB---_------_------_------_------";
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum StepResult {
+    /// The machine was in the Halt state. The tape remains unchanged.
     Halted,
+    /// The machine was not in the Halt state and is not in an empty [Action].
     Continue,
+    /// The machine was in an empty [Action]. The tape remains unchanged.
     Empty,
 }
 
+/// Simulate the [Tape] for one step according to the [Table]. If the Tape's state is [State::Halt]
+/// or if the [Action] supplied by the Table is empty, then nothing happens to the Tape
+/// (it is assumed that empty Actions correspond to a halt state). Otherwise, the appropriate Action
+/// is executed on the Tape.
 pub fn step(tape: &mut Tape, table: &Table) -> StepResult {
     let action = match (tape.state, tape.read()) {
         (State::A, Symbol::Zero) => table.state_a_0,
@@ -41,10 +57,15 @@ pub fn step(tape: &mut Tape, table: &Table) -> StepResult {
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum HaltReason {
-    Continue,
+    /// Machine halted because it entered the halt state.
     HaltState,
+    /// Machine halted because it entered an empty transition
     EmptyTransition,
+    /// Machine reached the space limit--that is, it visited more cells than the limit specified by
+    /// the maximum space limit.
     ExceededSpaceLimit,
+    /// Machine reached the step limit--that is, it was ran for more steps than the limit specified
+    /// by the maximum step limit.
     ExceededStepLimit,
 }
 
--- src/turing.rs
+++ src/turing.rs
@@ -1,17 +1,21 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
+/// The two symbols which can be written to the tape (zeros, or ones)
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Symbol {
     Zero,
     One,
 }
 
+/// The direction for the tape head to move on a step
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Direction {
     Left,
     Right,
 }
 
+/// The current state of the turing machine. Note that "Halt" is not considered to be a part of
+/// the normal states (any machine transitioning to Halt instantly Halts)
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum State {
     A,
@@ -22,11 +26,21 @@ pub enum State {
     Halt,
 }
 
+/// A transition edge in the transition table
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Transition(pub Symbol, pub Direction, pub State);
 
 pub type Action = Option<Transition>;
 
+/// The transition table. Note that this is written to allow for ease with enumerating transition
+/// tables. An [Action] which is None is used to represent that a particular transition is unusued or
+/// unreachable (and hence could be replaced by any Transition without affecting the behavior of the
+/// machine). When simulated on a [Tape], empty Actions are assumed to immediately halt without
+/// performing any changes to the Tape.
+///
+/// Each field in this table is named after the [State] + [Symbol] combination that the transition rule
+/// is for. For example, `state_d_1` is the transition rule for when the Turing machine reads a
+/// [Symbol::One] in [State::D]
 #[derive(Debug)]
 pub struct Table {
     pub state_a_0: Action,
@@ -42,6 +56,15 @@ pub struct Table {
 }
 
 impl Table {
+    /// Parse a Turing machine string in the following format:
+    /// `AAAaaa-BBBbbb-CCCccc-DDDddd-EEEeee`
+    /// Each triple of letters correspond to an transition. For instance, "aaa" is the [Action]
+    /// for the [State::A] + [Symbol::One] combination. A letter triple can either be `---`,
+    /// indicating that this is an empty Action, or has the following three charactes:
+    /// - First character: 0 or 1, representing the [Symbol] to write.
+    /// - Second character: L or R, representing the [Direction] for the tape head to move
+    /// - Third character: A, B, C, D, E, or Z, representing the [State] for the machine to
+    /// transition to. "Z" is assumed to be [State::Halt].
     pub fn parse(str: &str) -> Result<Table, ()> {
         fn parse_action(action: &str) -> Result<Action, ()> {
             if action.len() != 3 {
@@ -56,27 +79,28 @@ impl Table {
             let direction = action.next().unwrap();
             let state = action.next().unwrap();
 
-            let states = [
-                ('A', State::A),
-                ('B', State::B),
-                ('C', State::C),
-                ('D', State::D),
-                ('E', State::E),
-                ('Z', State::Halt),
-            ];
-            let states = HashMap::from(states);
+            let cell = match cell {
+                '0' => Ok(Symbol::Zero),
+                '1' => Ok(Symbol::One),
+                _ => Err(()),
+            }?;
 
-            let cell = if cell == '0' {
-                Symbol::Zero
-            } else {
-                Symbol::One
-            };
-            let direction = if direction == 'L' {
-                Direction::Left
-            } else {
-                Direction::Right
-            };
-            let state = *states.get(&state).unwrap();
+            let direction = match direction {
+                'L' => Ok(Direction::Left),
+                'R' => Ok(Direction::Right),
+                _ => Err(()),
+            }?;
+
+            let state = match state {
+                'A' => Ok(State::A),
+                'B' => Ok(State::B),
+                'C' => Ok(State::C),
+                'D' => Ok(State::D),
+                'E' => Ok(State::E),
+                'Z' => Ok(State::Halt),
+                _ => Err(()),
+            }?;
+
             Ok(Some(Transition(cell, direction, state)))
         }
 
@@ -117,14 +141,23 @@ impl Table {
     }
 }
 
+/// The tape, containing information needed to run a Turing machine.
+/// The tape consists of an infinitely long one-dimensional tape. The tape head, starts initially
+/// at index zero of this tape, and can move left or right. Cells to the left of the starting
+/// position have a negative index, while cells to the right have a positive index.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Tape {
+    /// A hashset containing the index of every [Symbol::One] written to the tape.
     pub ones: HashSet<isize>,
+    /// The current state of the machine
     pub state: State,
+    /// The location of the tape head
     pub index: isize,
 }
 
 impl Tape {
+    /// Construct a new empty tape. The tape will start in [State::A] and it's tape head will
+    /// be located at index 0.
     pub fn new() -> Tape {
         Tape {
             ones: HashSet::with_capacity(1024),
@@ -133,6 +166,7 @@ impl Tape {
         }
     }
 
+    /// Move the tape head one unit in the specified direction.
     pub fn shift(&mut self, direction: Direction) {
         match direction {
             Direction::Left => self.index -= 1,
@@ -140,6 +174,7 @@ impl Tape {
         }
     }
 
+    /// Write the specified [Symbol] to the cell at the tape head.
     pub fn write(&mut self, value: Symbol) {
         match value {
             Symbol::Zero => self.ones.remove(&self.index),
@@ -147,6 +182,7 @@ impl Tape {
         };
     }
 
+    /// Return the specified [Symbol] on the cell at the tape head.
     pub fn read(&self) -> Symbol {
         if self.ones.contains(&self.index) {
             Symbol::One
@@ -155,6 +191,7 @@ impl Tape {
         }
     }
 
+    /// Set the machine's [State]
     pub fn set_state(&mut self, state: State) {
         self.state = state;
     }
