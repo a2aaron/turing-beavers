3f71ad6623a66b6f623499a74ca18af736a114f8
Aaron Kofsky <aaronko@umich.edu>
2024-05-26 20:36:46 -0400

    Add perf.rs benchmark.

---
+++ src/bin/perf.rs
@@ -0,0 +1,31 @@
+use std::time::Instant;
+use turing_beavers::seed::Explorer;
+
+fn run(explorer: Explorer) {
+    while !explorer.machines_to_check.is_empty() {
+        if explorer.stats().decided() > NUM_MACHINES {
+            break;
+        }
+
+        let result = explorer.step_decide();
+        match result {
+            Some(_result) => continue,
+            None => break,
+        }
+    }
+}
+
+const NUM_MACHINES: usize = 70_000;
+
+fn main() {
+    println!("Deciding {} machines", NUM_MACHINES);
+    let now = Instant::now();
+    run(Explorer::new(1));
+    let elapsed = now.elapsed().as_secs_f32();
+    println!(
+        "Decided {} machines in {:.0} seconds ({:.0}/s",
+        NUM_MACHINES,
+        elapsed,
+        (NUM_MACHINES as f32 / elapsed)
+    );
+}
